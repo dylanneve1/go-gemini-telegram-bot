86c58bd410f84bf0968d39a8be476c60291b28cc
Dylan Neve <dylanneve1@gmail.com>
2024-04-21 12:46:34 +0100

    go-gemini: update to use gemini-1.5-pro-latest

--- pkg/model.go
+++ pkg/model.go
@@ -14,8 +14,8 @@ import (
 )
 
 const (
-	TextModel   = "gemini-pro"
-	VisionModel = "gemini-pro-vision"
+	TextModel   = "gemini-1.5-pro-latest"
+	VisionModel = "gemini-1.5-pro-latest"
 )
 
 var (
@@ -66,6 +66,10 @@ func InitModels() *genai.Client {
 	}
 	textModel.SafetySettings = SafetySettings
 	visionModel.SafetySettings = SafetySettings
+
+	textModel.SystemInstruction = &genai.Content{
+		Parts: []genai.Part{genai.Text("Your name is Gemini and you are running on a Telegram bot hosted by Dylan Neve.")},
+	}
 
 	modelMap[TextModel] = textModel
 	modelMap[VisionModel] = visionModel
